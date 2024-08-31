74ea8eefe6ae0411c930fc76325d3291144a73d2
swetha277 <210701277@rajalakshmi.edu.in>
2024-08-31 23:03:46 +0530

    Add files via upload

---
+++ Python/Prime Or Not Python/code.py
@@ -0,0 +1,17 @@
+def is_prime(number):
+    if number <= 1:
+        return False
+    if number == 2:
+        return True
+    if number % 2 == 0:
+        return False
+    for i in range(3, int(number**0.5) + 1, 2):
+        if number % i == 0:
+            return False
+    return True
+
+number = 9
+if PrimeNumbers(number):
+    print("Prime number")
+else:
+    print("Not a Prime")
---
+++ Python/multwonumcode.py
@@ -0,0 +1,6 @@
+def MultiplyTwoNumbers():
+	a = 10
+	b = 5
+	res = a*b
+	return str(res)
+
